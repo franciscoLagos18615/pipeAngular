ba7567d1c69eae5862b6c3872415fa1fb1b52262
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2018-10-18 19:46:19 -0300

    pipe  varios(json,etcmoneda,etc)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre = "Fernando" ;
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a: number = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "logan",
+    clave: "wolverine",
+    edad: 500,
+    direccion:{
+      calle:"primera",
+      casa:"19"
+    }
+
+  };
+
+  valorDePromesa = new Promise(  (resolve, reject) => {
+
+    setTimeout( () => resolve('llego la data'), 3500 );
+
+  });
+
 }
